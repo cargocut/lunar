830ace639ad3ba9377f86078656d8b3e9d46a8f0
xvw <xavier.vdw@gmail.com>
2026-02-25 09:24:16 +0100

    Start Date/Time and Datetime separation

---
+++ lib/date.ml
@@ -0,0 +1,120 @@
+(* Copyright (c) 2026, Cargocut and the Lunar developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+type t =
+  { year : int
+  ; month : Month.t
+  ; day_of_month : int
+  }
+
+type error =
+  | Invalid_year of int
+  | Invalid_month of Month.error
+  | Invalid_day of
+      { day_max : int
+      ; day : int
+      }
+
+exception Invalid_date of error
+
+let validate_year y =
+  if y < 0 || y > 9999
+  then
+    (* NOTE: Perhaps this constraint is a little too strict, but the
+       acceptable range of years seems reasonable for software development.*)
+    Error (Invalid_year y)
+  else Ok y
+;;
+
+let validate_day year month day =
+  let day_max = Month.days_in ~year month in
+  if day < 1 || day > day_max
+  then Error (Invalid_day { day_max; day })
+  else Ok day
+;;
+
+let make ~year ~month ~day () =
+  let ( let* ) = Result.bind in
+  let* year = validate_year year in
+  let* day_of_month = validate_day year month day in
+  Ok { year; month; day_of_month }
+;;
+
+let make_exn ~year ~month ~day () =
+  match make ~year ~month ~day () with
+  | Ok dt -> dt
+  | Error err -> raise (Invalid_date err)
+;;
+
+let make' ~year ~month ~day () =
+  let ( let* ) = Result.bind in
+  let* month =
+    Month.from_int month |> Result.map_error (fun e -> Invalid_month e)
+  in
+  make ~year ~month ~day ()
+;;
+
+let make_exn' ~year ~month ~day () =
+  match make' ~year ~month ~day () with
+  | Ok dt -> dt
+  | Error err -> raise (Invalid_date err)
+;;
+
+let from_duration d =
+  let year, month, day_of_month, _, _, _ = Duration.to_datetime d in
+  let month =
+    (* Result.get_ok should be safe because of
+       [to_datetime]. *)
+    month |> Month.from_int |> Result.get_ok
+  in
+  make_exn ~year ~month ~day:day_of_month ()
+;;
+
+let to_duration { year; month; day_of_month } =
+  let month = Month.to_int month in
+  Duration.from_datetime ~year ~month ~day:day_of_month ~hour:0 ~min:0 ~sec:0
+;;
+
+let epoch = from_duration Duration.zero
+let year { year; _ } = year
+let month { month; _ } = month
+let day_of_month { day_of_month; _ } = day_of_month
+let on_leap_year { year; _ } = Util.is_leap_year year
+
+let day_of_week dt =
+  let d = to_duration dt in
+  Duration.weekday d
+;;
+
+let day_of_year { year; month; day_of_month } =
+  let month_i = Month.to_int month
+  and offsets = [| 0; 31; 59; 90; 120; 151; 181; 212; 243; 273; 304; 334 |] in
+  let day_of_year = offsets.(pred month_i) + day_of_month in
+  if Month.(month > Feb) && Util.is_leap_year year
+  then day_of_year + 1
+  else day_of_year
+;;
+
+let iso_week_in_year year =
+  match
+    Duration.from_datetime ~year ~month:1 ~day:1 ~hour:0 ~min:0 ~sec:0
+    |> Duration.weekday
+  with
+  | Thu -> 53
+  | Wed when Util.is_leap_year year -> 53
+  | _ -> 52
+;;
+
+let week_of_year ({ year; _ } as d) =
+  let ts = to_duration d in
+  let iso_wd = (ts |> Duration.weekday |> Weekday.to_int) + 1
+  and day_y = day_of_year d in
+  let w = (day_y - iso_wd + 10) / 7 in
+  if w < 1
+  then (
+    let prev_year = year - 1 in
+    prev_year, iso_week_in_year prev_year)
+  else year, w
+;;
---
+++ lib/date.mli
@@ -0,0 +1,82 @@
+(* Copyright (c) 2026, Cargocut and the Lunar developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+(** Represents a date (year, month, day). *)
+
+(** {1 Types} *)
+
+(** The type describing a Date. *)
+type t
+
+(** Type listing errors that may occur when working with date. *)
+type error =
+  | Invalid_year of int
+  | Invalid_month of Month.error
+  | Invalid_day of
+      { day_max : int
+      ; day : int
+      }
+
+(** And exception used for unsafe function. *)
+exception Invalid_date of error
+
+(** {1 Building date} *)
+
+(** [make ~year ~month ~day ()] create and validate a date. *)
+val make : year:int -> month:Month.t -> day:int -> unit -> (t, error) result
+
+(** [make' ?at ~year ~month ~day ()] create and validate a datetime, see
+    {!val:make}. Take an integer rather than a {!val:Month.t}. *)
+val make' : year:int -> month:int -> day:int -> unit -> (t, error) result
+
+(** [make_exn ?at ~year ~month ~day ()] create and validate a date
+    like {!val:make} but raise [Invalid_date] if the validation
+    doesn't succeed. *)
+val make_exn : year:int -> month:Month.t -> day:int -> unit -> t
+
+(** [make_exn' ~year ~month ~day ()] create and validate a date,
+    like {!val:make'} but raise [Invalid_date] if the validation doesn't
+    succeed. *)
+val make_exn' : year:int -> month:int -> day:int -> unit -> t
+
+(** [from_duration d] converts a duration to a date.
+    [0] is the 1970-01-01. *)
+val from_duration : Duration.t -> t
+
+(** [epoch] is the date at 1970-01-01. *)
+val epoch : t
+
+(** {1 Accessors}
+
+    Information and facts about Date. *)
+
+(** [year dt] returns the year from a date. *)
+val year : t -> int
+
+(** [on_leap_year dt] returns [true] if [year dt] is a leap year, [false]
+    otherwise.*)
+val on_leap_year : t -> bool
+
+(** [month dt] returns the month from a date. *)
+val month : t -> Month.t
+
+(** [day_of_month dt] returns the day of month from a date. *)
+val day_of_month : t -> int
+
+(** [day_of_week dt] returns the day of week from a date. *)
+val day_of_week : t -> Weekday.t
+
+(** [day_of_year dt] returns the day of year from a date. *)
+val day_of_year : t -> int
+
+(** [week_of_year dt] returns the pair [year] and [week number] for a
+    given datetime [dt].
+
+    The ISO 8601 standard is used where Monday is the first
+    day of the week. The year is carried over if a day belongs
+    to a week in the previous year.
+
+    {b see:} {{:https://en.wikipedia.org/wiki/ISO_8601} ISO 8601}. *)
+val week_of_year : t -> int * int
