c3af2204e4afd5437d95485b29661c0e413cebe7
gr-im <grimfw@gmail.com>
2026-04-08 04:21:26 +0200

    fix #1
    Without implementation, the module `Lunar` is not _includable_ inside
    an executable.

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,7 @@
+### unreleased
+
+- Add an implementation for `Lunar` ([gr-im](https://github.com/gr-im))
+
 ### v1.0.0
 
 - First release of `lunar` exposing _among other things_ `Time`, `Date`, `Datetime`, `Timezone`, `Zoned_datetime` and `Range` (by [xvw](https://github.com/xvw), [gr-im](https://github.com/gr-im)).
---
+++ lib/lunar.ml
@@ -0,0 +1,17 @@
+(* Copyright (c) 2026, Cargocut and the Lunar developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+module Era = Era
+module Month = Month
+module Weekday = Weekday
+module Time = Time
+module Date = Date
+module Datetime = Datetime
+module Timezone = Timezone
+module Zoned_datetime = Zoned_datetime
+module Resolution = Resolution
+module Duration = Duration
+module Range = Range
+module Sigs = Sigs
